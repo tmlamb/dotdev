1067718495fe95aa616516285e080a4c050b4523
Tom Lamb <tmlamb@gmail.com>
2022-09-10 22:18:29 -0500

    corrected title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Triple<span className="text-purple-300">sight</span>
+          tmlamb<span className="text-purple-300">.dev</span>
         </h1>
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
