8ee936926b556540842520d11bcbce5341f53820
Tom Lamb <tmlamb@gmail.com>
2022-09-10 20:04:12 -0500

    workspace init

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return (
+    <>
+      <Head>
+        <title>dotdev</title>
+        <meta name="description" content="Just testing" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+          Triple<span className="text-purple-300">sight</span>
+        </h1>
+        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
