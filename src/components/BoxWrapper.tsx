6b88b74021ce01623cc17c50c0a9c1b8afe90077
Tom Lamb <tmlamb@gmail.com>
2022-09-11 22:09:06 -0500

    testing threejs

---
+++ src/components/BoxWrapper.tsx
@@ -0,0 +1,40 @@
+import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import * as THREE from "three";
+
+function Sphere(props: ThreeElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame((state, delta) =>
+    clicked
+      ? (ref.current.rotation.y += 0.01)
+      : (ref.current.rotation.x += 0.01)
+  );
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 3 : 2}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshStandardMaterial color={hovered ? "#93C5FD" : "red"} />
+    </mesh>
+  );
+}
+
+// createRoot(document.getElementById("root") as HTMLElement).render(
+export default function BoxWrapper() {
+  return (
+    <Canvas>
+      <ambientLight />
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <pointLight position={[10, 10, 10]} intensity={3} />
+      <Sphere position={[0, 0, 0]} />
+    </Canvas>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { trpc } from "../utils/trpc";
+import BoxWrapper from "../components/BoxWrapper";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
   return (
     <>
       <Head>
@@ -13,12 +11,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          tmlamb<span className="text-purple-300">.dev</span>
+      <main className="container mx-auto flex flex-col items-center justify-center space-y-10 min-h-screen p-4">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl leading-normal font-extrabold text-gray-700">
+          tmlamb<span className="text-blue-400">dotdev</span>
         </h1>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+        <div className="h-full w-full">
+          <BoxWrapper />
         </div>
       </main>
     </>
