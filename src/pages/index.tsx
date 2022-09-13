fa1ab180795ffb681e339a62a4b2005783865948
Tom Lamb <tmlamb@gmail.com>
2022-09-13 10:08:24 -0500

    h1 change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className="container mx-auto flex flex-col items-center justify-center space-y-10 min-h-screen p-4">
         <h1 className="text-3xl md:text-5xl lg:text-6xl leading-normal font-extrabold text-gray-700">
-          tmlamb<span className="text-blue-400">dotdev</span>
+          <span className="text-blue-400">dotdev</span>
         </h1>
         <div className="h-full w-full">
           <BoxWrapper />
